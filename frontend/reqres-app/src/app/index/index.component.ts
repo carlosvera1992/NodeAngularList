8ed65a90c0cd8893d84cc551c66fb1f53046cc6e
carlos Vera <monovera501@gmail.com>
2020-06-01 07:46:16 -0500

    Improving the design

--- frontend/reqres-app/src/app/index/index.component.ts
+++ frontend/reqres-app/src/app/index/index.component.ts
@@ -19,12 +19,6 @@ export class IndexComponent implements OnInit {
   getUsers(){
     this.reqresService.getUsers().subscribe(
       res => {
-        //La variable res contiene la respuesta del backend
-        //data en el JSON contiene la lsita de usuarios
-        console.log(res["data"]);
-
-        //haré que la data vaya a una variable para iterar sobre ella en una
-        //tabla en el HTML correspondiente index.component.html
         this.users = res["data"];
       },
       err => {
--- frontend/reqres-app/src/app/user/user.component.ts
+++ frontend/reqres-app/src/app/user/user.component.ts
@@ -30,22 +30,13 @@ export class UserComponent implements OnInit {
   getUserById(idUser){
     this.reqresService.getUserById(idUser).subscribe(
       res => {
-        //La variable res contiene la respuesta del backend
-        //Checa como sacar cada valor dentro del JSON de respuesta
-        console.log(res["data"]);
-        console.log(res["data"].id);
-        console.log(res["data"].email);
-        console.log(res["data"].first_name);
-        console.log(res["data"].last_name);
-        console.log(res["data"].avatar);
-
-        //mira como igualo lo que viene del JSON y lo paso a una variable "name"
+      
+        this.idUser = res["data"].id;
         this.email = res["data"].email;
         this.firstName = res["data"].first_name;
         this.lastName = res["data"].last_name;
         this.avatar = res["data"].avatar;
-
-        //Ahora mira el HTML correspondiente user.component.html
+        
       },
       err => {
 
